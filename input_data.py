0d728b731e9369fe812fdabc608c5ccfad7b89b4
shiozaki <shiozaki@egw.co.jp>
2023-09-25 19:02:51 +0900

    データベースの修正

--- input_data.py
+++ input_data.py
@@ -1,12 +1,32 @@
 import PySimpleGUI as sg
+import sqlite3
 from select_item import SelectItem
 
 sg.theme("SystemDefault")
-DATABASE = "database/vehicle.db"
+DATABASE = "database/database.db"
 SQL_DEPT = "SELECT code, name FROM M部署;"
 SQL_SITUATION = "SELECT code, name FROM M状況;"
 SQL_CARSIZE = "SELECT code, name FROM M車格;"
 SQL_CARTYPE = "SELECT code, name FROM M種別;"
+
+
+def get_id_number(table_name):
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    sql1 = f"""
+        SELECT * FROM {table_name};
+        """
+    cur.execute(sql1)
+    if len(cur.fetchall()) == 0:
+        new_id = 1
+    else:
+        sql = f"""
+            SELECT max(id) FROM {table_name};
+            """
+        cur.execute(sql)
+        result = cur.fetchone()
+        new_id = result[0] + 1
+    return new_id
 
 
 # ウインドウレイアウト
@@ -51,6 +71,8 @@ layout = [[sg.T("車両入力", font=("Yu Gothic UI", 11))],
          sg.I(k="-in11-", size=(10, 0), font=("Yu Gothic UI", 8))],
         [sg.T("燃料種類", size=(10, 0), font=("Yu Gothic UI", 8)),
          sg.I(k="-in12-", size=(20, 0), font=("Yu Gothic UI", 8))],
+        [sg.T("実施日", size=(10, 0), font=("Yu Gothic UI", 8)),
+         sg.I(k="-in13-", size=(20, 0), font=("Yu Gothic UI", 8))],
         [sg.Frame(title="登録番号", font=("Yu Gothic UI", 8), layout=frame_layout)],
         [sg.B("検索", k="-btn_search-", size=(10, 0), font=("Yu Gothic UI", 8)),
          sg.Push(),
@@ -79,6 +101,22 @@ while True:
         if items:
             window["-in8-"].update(items[1])
             window["-cd3-"].update(items[0])
+    if e == "-btn_register-":
+        new_id = get_id_number()
+        date = 
+        car_info = []
+        car_info.append(new_id)         # T車両台帳<id>
+        car_info.append(v["-in1-"])     # T車両台帳<company_use_number>
+        car_info.append(v["-cd2-"])     # T車両台帳<classification>
+        car_info.append(v["-in4-"])     # T車両台帳<maker>
+        car_info.append(v["-in5-"])     # T車両台帳<model_number>
+        car_info.append(v["-in6-"])     # T車両台帳<body_number>
+        car_info.append(v["-in7-"])     # T車両台帳<model_year>
+        car_info.append(v["-cd3-"])     # T車両台帳<car_size>
+        car_info.append(v["-in9-"])     # T車両台帳<load_capacity>
+        car_info.append(v["-in10-"])    # T車両台帳<specification>
+        car_info.append(v["-in11-"])    # T車両台帳<riding_capacity>
+        car_info.append(v["-in12-"])    # T車両台帳<fuel_type>
     if e == None:
         break
 window.close()
